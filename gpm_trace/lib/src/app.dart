d81ed7420e459afe8921306e1a9b4f93f609d161
Jpgotopo <jpgotopo@GMAIL.COM>
2019-09-28 15:49:03 -0500

    modificando app.dart

--- gpm_trace/lib/src/app.dart
+++ gpm_trace/lib/src/app.dart
@@ -1,30 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flare_flutter/flare_actor.dart';
-
+import 'package:gpm_trace/src/pages/signup.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('GPM Tracking'),
-        centerTitle: true,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
 
-      ),
-      body: Container(
-        height: 350,
-        child: FlareActor(
-          "lib\src\assets\GPM Tracing.flr",
-          animation: "entrada",
-          fit: BoxFit.fitWidth,
-        ),
-      ),
-    );
-
-     /*  home: Center(
+      home: Center(
         child: SignUp(),
-      ) */
+      )
 
-    
+    );
   }
 }
